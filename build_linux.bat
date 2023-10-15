3574aae9d21eaffb5d2de5c423c911dc381035d7
unknown <1570693659@qq.com>
2023-10-15 18:11:33 +0800

    feat:init

--- build_linux.bat
+++ build_linux.bat
@@ -1,10 +1,9 @@
-SET CGO_ENABLED=1
+SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
-SET CC=x86_64-w64-mingw32-gcc
-SET CXX=x86_64-w64-mingw32-g++
-SET CGO_LDFLAGS="-static"
-echo now the CGO_ENABLED:
+SET CC=gcc
+SET CXX=g++
+SET CGO_LDFLAGS="-g -O2"
 go env CGO_ENABLED
 
 echo now the GOOS:
